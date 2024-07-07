4d76dd3f88d96cc089adcaa7c42f484a5aacbd52
Orniepay <payerornie@gmail.com>
2024-07-07 17:09:22 -0400

    Learning Vanilla Java Script Document Object Model Part 3: Events - add event listeners with click, dblclick, mousedown, mouseup, mouseenter, mouseleave, mouseoverm mouseout, mousemove, keydown, keyup, keypress, focus, blur, cut, paste, input, etc.

--- dom.js
+++ dom.js
@@ -193,3 +193,92 @@ IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES */
 
 
     // console.log(newDiv);
+
+//------------------------------------------------------------------------------------------------------//
+
+/* EVENTS */ 
+
+// var button = document.getElementById('button').addEventListener('click', buttonClick);
+
+// var button = document.getElementById('button').addEventListener('click', function(){
+//     console.log(123);
+// });
+
+// function buttonClick(){
+//     // console.log('Button clicked');
+//     document.getElementById('header-title').textContent = 'Changed';
+//     document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+// }
+
+// Experimenting :
+// function buttonClick(e){
+//     console.log(e);
+//     console.log(e.target.id);
+//     console.log(e.target.className);
+//     console.log(e.target.classList);
+
+//     var output = document.getElementById('output');
+//     output.innerHTML = '<h3> +e.target.id+ </h3>';
+
+//     console.log(e.clientX);
+//     console.log(e.clientY);
+
+//     console.log(e.offsetX);
+//     console.log(e.offsetY);
+
+//     console.log(e.altKey);
+//     console.log(e.ctrlKey);
+//     console.log(e.shiftKey);
+// }
+
+// var button = document.getElementById('button').addEventListener('click', runEvent);
+// var box = document.getElementById('box').addEventListener('click', runEvent);
+
+// Same thing as above. But, disconnected. 
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent)
+
+// var itemInput = document.querySelector('input[type="text"]');
+// var form = document.querySelector('form');
+// var select = document.querySelector('select');
+
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+// itemInput.addEventListener('input', runEvent);
+
+// select.addEventListener('change', runEvent);
+// select.addEventListener('input', runEvent);
+
+// form.addEventListener('submit', runEvent);
+
+// function runEvent(e){
+//     e.preventDefault();
+//     console.log('EVENT TYPE: ' +e.type);
+
+    // console.log(e.target.value);
+    // document.getElementById('output').innerHTML = '<h3>' +e.target.value+'</h3>';
+
+    // output.innerHTML = '<h3>MouseX: '+e.offsetX+'</h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+    // document.body.style.backgroundColor = 'rgb('+e.offset+','+e.offsetY+',40)';
+// }
+
+//----------------------------------------------------------------------------------------------------------//
